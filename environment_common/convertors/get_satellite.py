febeabe75a7fe41544f9e410be1573479dfb76bb
James R Heselden <primordia@live.com>
2023-06-28 20:48:06 +0100

    tmap to klm included

--- environment_common/convertors/get_satellite.py
+++ environment_common/convertors/get_satellite.py
@@ -6,8 +6,60 @@
 # @date:
 # ----------------------------------
 
+import os, subprocess
+import ee
+
 def main(args=None):
-    print('main function')
+    pass
+if True:
+    # Initialize credentials
+    cred_id = os.getenv('GCLOUD_SERVICE_ID')
+    cred_key_file = os.getenv('GCLOUD_SERVICE_KEY')
+    credentials = ee.ServiceAccountCredentials(cred_id, cred_key_file)
+    ee.Initialize(credentials)
+
+    # Specify GPS location
+    u_lat = -70.892
+    #53.26851890186007
+    u_lon = 41.6555
+    #-0.5240515126879408
+    u_poi = ee.Geometry.Point(u_lon, u_lat)
+    roi = u_poi.buffer(1e6)
+
+    # Specify search meta
+    i_date = '2014-01-01'
+    f_date = '2016-01-01'
+
+    #https://developers.google.com/earth-engine/tutorials/community/intro-to-python-api
+
+    # Specify image dataset
+    imgc = ee.ImageCollection('SKYSAT/GEN-A/PUBLIC/ORTHO/RGB')
+    #https://developers.google.com/earth-engine/datasets/catalog/SKYSAT_GEN-A_PUBLIC_ORTHO_RGB
+
+
+    # Filter the collection to our requirements
+    #imgc = imgc.filterDate(i_date, f_date)
+    r = imgc.select('R')
+    g = imgc.select('G')
+    b = imgc.select('B')
+    img = r.first()
+
+    #imgc = ee.ImageCollection('MODIS/006/MOD11A1')
+    #lst = imgc.select('LST_Day_1km', 'QC_Day').filterDate(i_date, f_date)
+    #lst_img = lst.mean()
+    #lst_img = lst_img.select('LST_Day_1km').multiply(0.02)
+    #lst_img = lst_img.select('LST_Day_1km').add(-273.15)
+    #img = lst_img
+
+    # Generate URL link
+    url = img.getThumbUrl({'min': 11, 'max': 190, 'dimensions': 512, 'region': roi})
+    print(url)
+
+
+    # Download file from Link
+    #subprocess.run(f'wget {link}')
+
 
 if __name__ == '__main__':
-    main()
+    main(args={})
+
--- environment_common/convertors/tmap_as_klm.py
+++ environment_common/convertors/tmap_as_klm.py
@@ -67,8 +67,18 @@ template_end = """
 </kml>"""
 
 
+e = 'environment_template'
+src = '/'.join(get_package_prefix(e).split('/')[:-2]) + f'/src/{e}'
+location_name = 'riseholme_polytunnel'
+args = {'src': src, 'location_name':location_name, 'line_col':'ff2f2fd3', 'line_width':'6', 'fill_col':'c02f2fd3'}
 
 def main(args=None):
+    pass
+if True:
+    line_col = args['line_col']
+    line_width = args['line_width']
+    fill_col = args['fill_col']
+
     datum_path = os.path.join(args['src'], 'config', 'location', 'datum.yaml')
     with open(datum_path) as f:
         data = f.read()
@@ -77,24 +87,49 @@ def main(args=None):
     place_id = args['location_name']
     lat = datum['datum_latitude']
     lon = datum['datum_longitude']
-    alt = 0
 
-    fency = datum['gnss_fence']['gnss_fence_coords']
-    fency += [datum['gnss_fence']['gnss_fence_coords'][0]]
-    fence = ' '.join([f'{g[1]},{g[0]},{alt}' for g in fency])
+    tmap_path = os.path.join(args['src'], 'config', 'topological', 'network.tmap2')
+    with open(tmap_path) as f:
+        data = f.read()
+        tmap = yaml.safe_load(data)
 
-    line_col = args['line_col']
-    line_width = args['line_width']
-    fill_col = args['fill_col']
+    node_pose_list = dict()
+    for n in tmap['nodes']:
+        node_pose_list[n['node']['name']] = n['node']['pose']['position']
 
-    klm = template_start % f"{place_id}_klm_autogen"
+    edge_list = set()
+    for n in tmap['nodes']:
+        for e in n['node']['edges']:
+            l = [e['node'],n['node']['name']]
+            l.sort()
+            edge_list.add(f"{l[0]}_{l[1]}")
+
+    klm = template_start % f"{place_id}_tmap_klm_autogen"
     klm += template_style % ("a1",  line_col, line_width, fill_col)
     klm += template_style % ("a2", line_col, line_width, fill_col)
     klm += template_style_join % ("a", "a1", "a2")
-    klm += template_placemark % ("a0", place_id, lon, lat, alt, "a", fence)
+
+    from math import cos
+    def add_to_gps(latitude, longitude, node_pose_list, node):
+        x_offset = node_pose_list[node]['x'] / (cos(latitude) * 111111)
+        y_offset = node_pose_list[node]['y'] / (111111)
+        return latitude + y_offset, longitude - (x_offset)
+
+    for i,e in enumerate(edge_list):
+        s, t = e.split('_')
+
+        # Get latlon corrections
+        lat_s, lon_s = add_to_gps(lat, lon, node_pose_list, s)
+        lat_t, lon_t = add_to_gps(lat, lon, node_pose_list, t)
+
+        fence = f"{lon_s},{lat_s},0 {lon_t},{lat_t},0"
+        klm += template_placemark % ("a0", e, lon_s, lat_s, 0, "a", fence)
+
+    fence = f"{lon},{lat-0.00001},0 {lon},{lat},0"
+    klm += template_placemark % ("a0", "center", lon, lat, 0, "a", fence)
     klm += template_end
 
-    klm_path = os.path.join(args['src'], 'config', 'location', 'region.klm')
+    klm_path = os.path.join(args['src'], 'config', 'topological', 'tmap2.klm')
     with open(klm_path, 'w') as f:
         f.write(klm)
 
@@ -103,5 +138,5 @@ if __name__ == '__main__':
     e = 'environment_template'
     src = '/'.join(get_package_prefix(e).split('/')[:-2]) + f'/src/{e}'
     location_name = 'riseholme_polytunnel'
-    main({'src': src, 'location_name':location_name, 'line_col':'ff2f2fd3', 'line_width':'24', 'fill_col':'c02f2fd3'})
+    main({'src': src, 'location_name':location_name, 'line_col':'40ffffff', 'line_width':'24', 'fill_col':'40ffffff'})
 
