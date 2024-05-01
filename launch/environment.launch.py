b0eb1629be985215b8e17d8b27aa8319f040d544
James R Heselden <primordia@live.com>
2024-05-01 12:28:59 +0100

    rviz arg included to launch file

--- launch/environment.launch.py
+++ launch/environment.launch.py
@@ -24,6 +24,7 @@ def either(folder, file1, file2):
     return f"{folder}/{file1}" if os.path.exists(f"{folder}/{file1}") else f"{folder}/{file2}"
 
 def generate_launch_description():
+    CONF = join(get_package_share_directory('environment_common'), 'config')
     ENV = join(get_package_share_directory('environment_template'), 'config')
     LD = LaunchDescription()
 
@@ -31,20 +32,30 @@ def generate_launch_description():
     metric_default_value = either(join(ENV, 'metric', 'map'), 'map.yaml', 'map_autogen.yaml')
     tmap_default_value = either(join(ENV, 'topological'), 'network.tmap2.yaml', 'network_autogen.tmap2.yaml')
     gazebo_default_value = either(join(ENV, 'world'), 'gazebo.world', 'gazebo_autogen.world')
+    #
+    rviz_default_value = join(CONF, 'topomap_marker.rviz')
+
 
     # Declare the launch arguments
     LD.add_action(DeclareLaunchArgument('map', default_value=metric_default_value))
     LD.add_action(DeclareLaunchArgument('tmap', default_value=tmap_default_value))
     LD.add_action(DeclareLaunchArgument('world', default_value=gazebo_default_value))
+    #
+    LD.add_action(DeclareLaunchArgument('rviz', default_value=rviz_default_value))
+
 
     # Define references for the arg inputs to be saved
     map_input = LaunchConfiguration('map')
     tmap_input = LaunchConfiguration('tmap')
     gazebo_input = LaunchConfiguration('world')
+    #
+    rviz_input = LaunchConfiguration('rviz')
+
 
     # Declare namespace
     #namespace = LaunchConfiguration('namespace')
     #LD.add_action(DeclareLaunchArgument('namespace', default_value=namespace, description='Top-level namespace'))
+
 
     # Display status
     print('\n'*5)
@@ -58,6 +69,7 @@ def generate_launch_description():
     print('Loading gazebo:', gazebo_input)
     print('--'*10)
     print('\n'*5)
+
 
     ## Topological Map Server
     LD.add_action(Node(
@@ -76,6 +88,7 @@ def generate_launch_description():
         executable='topomap_marker2.py',
         name='topomap_marker2'
     ))
+
 
     # Define map node
     """
@@ -107,8 +120,10 @@ def generate_launch_description():
     ## RViz2
     LD.add_action(Node(
         package='rviz2',
-        executable='rviz2'
+        executable='rviz2',
+        arguments=['-d', rviz_input]
     ))
+
 
     ## Execute all Components
     return LD
